1060cacff36c12c4cb14f9af4f61bf2f08394baa
Leonardo Val <leonardo.val@creatartis.com>
2018-10-31 14:17:27 -0300

    New build

--- build/ludorum-game-chess.js
+++ build/ludorum-game-chess.js
@@ -33,7 +33,9 @@ function __init__(base, Sermat, ludorum, ChessJS) { "use strict";
 			__name__: 'ludorum_game_chess',
 			__init__: __init__,
 			__dependencies__: [base, Sermat, ludorum, chessjs_package],
-			__SERMAT__: { include: [base, ludorum] }
+			__SERMAT__: { include: [base, ludorum] },
+			// Submodules
+			ai: { }
 		};
 
 
@@ -158,16 +160,7 @@ var Chess = exports.Chess = declare(ludorum.Game, {
 			}
 			return r;
 		};
-	})(),
-
-	// ## Heuristics ###############################################################################
-
-	/** `Chess.heuristics` is a bundle of helper functions to build heuristic evaluation functions
-	for this game.
-	*/
-	'static heuristics': {
-		// TODO
-	}
+	})()
 }); // declare Othello.
 
 /** Adding Chess to `ludorum.games`.
@@ -180,6 +173,111 @@ Chess.__SERMAT__.identifier = exports.__package__ +'.'+ Chess.__SERMAT__.identif
 exports.__SERMAT__.include.push(Chess);
 Sermat.include(exports);
 
+
+/** # Heuristic 1  
+
+Game evaluation heuristic based on a [Lauri Hartikka's blog post](https://medium.freecodecamp.org/simple-chess-ai-step-by-step-1d55a9266977).
+*/
+exports.ai.heuristic1 = (function () {
+	var BOARD_FACTORS = Object.freeze({
+			p: [
+				[ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+				[+5.0,+5.0,+5.0,+5.0,+5.0,+5.0,+5.0,+5.0],
+				[+1.0,+1.0,+2.0,+3.0,+3.0,+2.0,+1.0,+1.0],
+				[+0.5,+0.5,+1.0,+2.5,+2.5,+1.0,+0.5,+0.5],
+				[ 0.0, 0.0, 0.0, 2.0, 2.0, 0.0, 0.0, 0.0],
+				[+0.5,-0.5,-1.0, 0.0, 0.0,-1.0,-0.5,+0.5],
+				[+0.5,+1.0,+1.0,-2.0,-2.0,+1.0,+1.0,+0.5],
+				[ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+			],
+			n: [
+				[-5.0,-4.0,-3.0,-3.0,-3.0,-3.0,-4.0,-5.0],
+				[-4.0,-2.0, 0.0, 0.0, 0.0, 0.0,-2.0,-4.0],
+				[-3.0, 0.0,+1.0,+1.5,+1.5,+1.0, 0.0,-3.0],
+				[-3.0,+0.5,+1.5,+2.0,+2.0,+1.5,+0.5,-3.0],
+				[-3.0, 0.0,+1.5,+2.0,+2.0,+1.5, 0.0,-3.0],
+				[-3.0,+0.5,+1.0,+1.5,+1.5,+1.0,+0.5,-3.0],
+				[-4.0,-2.0, 0.0,+0.5,+0.5, 0.0,-2.0,-4.0],
+				[-5.0,-4.0,-3.0,-3.0,-3.0,-3.0,-4.0,-5.0]
+			],
+			b: [
+				[-2.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-2.0],
+				[-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-1.0],
+				[-1.0, 0.0,+0.5,+1.0,+1.0,+0.5, 0.0,-1.0],
+				[-1.0,+0.5,+0.5,+1.0,+1.0,+0.5,+0.5,-1.0],
+				[-1.0, 0.0,+1.0,+1.0,+1.0,+1.0, 0.0,-1.0],
+				[-1.0,+1.0,+1.0,+1.0,+1.0,+1.0,+1.0,-1.0],
+				[-1.0,+0.5, 0.0, 0.0, 0.0, 0.0,+0.5,-1.0],
+				[-2.0,-1.0,-1.0,-1.0,-1.0,-1.0,-1.0,-2.0]
+			],
+			r: [
+				[ 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
+				[ 0.5,+1.0,+1.0,+1.0,+1.0,+1.0,+1.0,+0.5],
+				[-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.5],
+				[-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.5],
+				[-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.5],
+				[-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.5],
+				[-0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-0.5],
+				[ 0.0, 0.0, 0.0,+0.5,+0.5, 0.0, 0.0, 0.0]
+			],
+			q: [
+				[-2.0,-1.0,-1.0,-0.5,-0.5,-1.0,-1.0,-2.0],
+				[-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,-1.0],
+				[-1.0, 0.0,+0.5,+0.5,+0.5,+0.5, 0.0,-1.0],
+				[-0.5, 0.0,+0.5,+0.5,+0.5,+0.5, 0.0,-0.5],
+				[ 0.0, 0.0,+0.5,+0.5,+0.5,+0.5, 0.0,-0.5],
+				[-1.0,+0.5,+0.5,+0.5,+0.5,+0.5, 0.0,-1.0],
+				[-1.0, 0.0,+0.5, 0.0, 0.0, 0.0, 0.0,-1.0],
+				[-2.0,-1.0,-1.0,-0.5,-0.5,-1.0,-1.0,-2.0]
+			],
+			k: [
+				[-3.0,-4.0,-4.0,-5.0,-5.0,-4.0,-4.0,-3.0],
+				[-3.0,-4.0,-4.0,-5.0,-5.0,-4.0,-4.0,-3.0],
+				[-3.0,-4.0,-4.0,-5.0,-5.0,-4.0,-4.0,-3.0],
+				[-3.0,-4.0,-4.0,-5.0,-5.0,-4.0,-4.0,-3.0],
+				[-2.0,-3.0,-3.0,-4.0,-4.0,-3.0,-3.0,-2.0],
+				[-1.0,-2.0,-2.0,-2.0,-2.0,-2.0,-2.0,-1.0],
+				[+2.0,+2.0, 0.0, 0.0, 0.0, 0.0,+2.0,+2.0],
+				[+2.0,+3.0,+1.0, 0.0, 0.0,+1.0,+3.0,+2.0]
+			]
+		}),
+		PIECE_FACTORS = Object.freeze({
+			p: 10.0,
+			n: 30.0,
+			b: 30.0,
+			r: 50.0,
+			q: 90.0,
+			k: 900.0
+		});
+
+	var makeEvaluationFunction = function makeEvaluationFunction(boardFactors, pieceFactors) {
+		boardFactors = boardFactors || BOARD_FACTORS;
+		pieceFactors = pieceFactors || PIECE_FACTORS;
+		//TODO Calculate max result for normalization.
+		return function chessHeuristic(game, role) {
+			var result = 0, 
+				sq;
+			for (var row = 0; row < 8; row++) {
+				for (var col = 0; col < 8; col++) {
+					sq = game.square([row, col]);
+					if (sq) {
+						//FIXME Check player's role.
+						result += pieceFactors[sq.type] * (sq.color === 'w' ? 
+							boardFactors[row][col] : boardFactors[8 - row][col]);
+					}
+				}
+			}
+			return result; //FIXME Normalize!
+		};
+	};
+
+	return {
+		BOARD_FACTORS: BOARD_FACTORS,
+		PIECE_FACTORS: PIECE_FACTORS,
+		makeEvaluationFunction: makeEvaluationFunction
+		//TODO Player building functions (MiniMax, etc).
+	};
+})();
 
 // See __prologue__.js
 	return exports;
